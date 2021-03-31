ab214fa751e78893747ccac8169af7ca50b6a2eb
DyllanRuth <73084302+DyllanRuth@users.noreply.github.com>
2021-03-31 08:50:33 -0500

    Added/Created LobbyPages, Fixed Typo

--- lib/lobby-screen.dart
+++ lib/lobby-screen.dart
@@ -1,13 +1,117 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Lobby extends StatefulWidget {
-  @override
-  _LobbyState createState() => _LobbyState();
-}
 
-class _LobbyState extends State<Lobby> {
+class JoinLobby extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Color(0xff010f32),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 130,
+                ),
+                RichText(
+                    text: TextSpan(
+                        style: TextStyle(fontSize: 45),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Gamepin:',
+                              style: TextStyle(
+                                  color: Colors.white, fontWeight: FontWeight.bold)),
+                          TextSpan(
+                              text: ' 213434',
+                              style: TextStyle(
+                                  color: Color(0xff04E9CD),
+                                  fontWeight: FontWeight.bold))
+                        ])),
+                SizedBox(height: 20),
+                RichText(
+                    text: TextSpan(
+                        style: TextStyle(fontSize: 35),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Lobby',
+                              style: TextStyle(
+                                  color: Colors.white, fontWeight: FontWeight.bold)),
+                        ])),
+                SizedBox(height: 30,),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white
+                  ),
+                  child: Column(
+                    children: <Widget>[
+
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+                      ///////////////////////////////////////////
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.grey[300]))
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+
+                        ),
+
+                        child: Center(child: Text("Jannet", textAlign: TextAlign.center,),),
+                      ),
+
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ));
+  }}
