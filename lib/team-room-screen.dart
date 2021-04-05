43598de04bb087d096a30ffe9c891626c690e910
omenrobot12 <omenrobot12@gmail.com>
2021-04-04 22:27:25 -0500

    add back button widget  and geolocation

--- lib/team-room-screen.dart
+++ lib/team-room-screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'create-room.dart';
 import 'lobby-screen.dart';
+import './back-button.dart';
+
 
 class Team extends StatefulWidget {
   @override
@@ -16,93 +18,99 @@ class _TeamState extends State<Team> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff000C2D),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 200,
-            ),
-            RichText(
-                text: TextSpan(
-                    style: TextStyle(fontSize: 45),
-                    children: <TextSpan>[
-                  TextSpan(
-                      text: 'Team',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold)),
-                  TextSpan(
-                      text: 'Room',
-                      style: TextStyle(
-                          color: Color(0xff04E9CD),
-                          fontWeight: FontWeight.bold))
-                ])),
-            SizedBox(height: 30),
-            Container(
-              child: TextField(
-                textAlign: TextAlign.center,
-                controller: _controller,
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  hintStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    //borderRadius: BorderRadius.all(Radius.circular(30))
+      backgroundColor: Color(0xff000C2D),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+              ),
+              RichText(
+                  text: TextSpan(
+                      style: TextStyle(fontSize: 45),
+                      children: <TextSpan>[
+                    TextSpan(
+                        text: 'Team',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text: 'Room',
+                        style: TextStyle(
+                            color: Color(0xff04E9CD),
+                            fontWeight: FontWeight.bold))
+                  ])),
+              SizedBox(height: 30),
+              Container(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      //borderRadius: BorderRadius.all(Radius.circular(30))
+                    ),
                   ),
                 ),
+                padding: EdgeInsets.symmetric(horizontal: 32),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 32),
-            ),
-            Container(
-              child: TextField(
-                textAlign: TextAlign.center,
-                controller: _controller,
-                decoration: InputDecoration(
-                  hintText: 'Game Pin',
-                  hintStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    //borderRadius: BorderRadius.all(Radius.circular(30))
+              Container(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Game Pin',
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      //borderRadius: BorderRadius.all(Radius.circular(30))
+                    ),
                   ),
                 ),
+                padding: EdgeInsets.symmetric(horizontal: 32),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 32),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            Container(
-              child: TextButton(
-                style: TextButton.styleFrom(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 159, vertical: 18),
-                    backgroundColor: Color(0xff04E9CD),
-                    primary: Colors.black),
-                child: Text("JOIN"),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => JoinLobby()));
-                },
+              SizedBox(
+                height: 32,
               ),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            Container(
-              child: TextButton(
-                style: TextButton.styleFrom(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 159, vertical: 18),
-                    backgroundColor: Colors.pink,
-                    primary: Colors.black),
-                child: Text("HOST"),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => CreateRoom()));
-                },
+              Container(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 159, vertical: 18),
+                      backgroundColor: Color(0xff04E9CD),
+                      primary: Colors.black),
+                  child: Text("JOIN"),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => JoinLobby()));
+                  },
+                ),
               ),
-            )
-          ],
-        ));
+              SizedBox(
+                height: 32,
+              ),
+              Container(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 159, vertical: 18),
+                      backgroundColor: Colors.pink,
+                      primary: Colors.black),
+                  child: Text("HOST"),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => CreateRoom()));
+                  },
+                ),
+              )
+            ],
+          ),
+          BackButtonSPS(),
+        ],
+      ),
+    );
   }
 }
